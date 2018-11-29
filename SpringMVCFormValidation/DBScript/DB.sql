239fa4eb86bdce430e622e9428cb5d6db095316d
pathik10patel <pathik10patel@gmail.com>
2018-11-29 21:46:42 +0530

    Update DB.sql

--- SpringMVCFormValidation/DBScript/DB.sql
+++ SpringMVCFormValidation/DBScript/DB.sql
@@ -1,12 +1,7 @@
-CREATE TABLE `student_table` (
-  `id` int(11) NOT NULL auto_increment,
-  `student_name` varchar(100) NOT NULL,
-  `gender` varchar(10) default NULL,
-  `technology` varchar(100) NOT NULL,
-  `city` varchar(100) default NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(20) default NULL,
-  `phone` bigint(20) default NULL,
-  PRIMARY KEY  (`id`)
-) ;
-
+create table books1
+(id int,
+ bookname varchar(50),
+ bookid varchar(50),
+ author varchar(50),
+ primary key(bookname)
+);
